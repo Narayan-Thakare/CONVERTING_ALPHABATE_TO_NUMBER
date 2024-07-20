83de6bae52a7dee482d9e50550ef20b2acd0855c
Narayan Thakare <narayan111thakare@gmail.com>
2024-07-20 20:54:14 +0530

    first commit

---
+++ src/Converting.java
@@ -0,0 +1,17 @@
+
+public class Converting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char ch = 'N';
+		int i = ch ;
+		System.out.println(i);
+		
+		
+		
+		
+
+	}
+
+}
